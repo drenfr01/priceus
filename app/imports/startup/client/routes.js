4f5b99bf31d8ed800a58c125122808f49d26f0f5
Tyler Sheffels <tyler.sheffels@gmail.com>
2016-04-10 17:35:04 -0400

    Uncomment search route:

--- app/imports/startup/client/routes.js
+++ app/imports/startup/client/routes.js
@@ -3,16 +3,16 @@ import React from 'react';
 import {mount} from 'react-mounter';
 
 // load React components
-// import {SearchLayout, Search} from '../../ui/components/search.jsx';
+import {SearchLayout, Search} from '../../ui/components/search.jsx';
 import {AddPriceLayout, AddPrice} from '../../ui/components/addPrice.jsx';
 
-// FlowRouter.route("/", {
-//   action() {
-//     mount(SearchLayout, {
-//       content: (<Search/>)
-//     });
-//   }
-// });
+FlowRouter.route("/", {
+  action() {
+    mount(SearchLayout, {
+      content: (<Search/>)
+    });
+  }
+});
 
 FlowRouter.route("/add-price", {
   action() {
